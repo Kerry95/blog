9fc30030abf8fea103227928b15ad3b7ffee8936
Kerry Penaluna <kez.penaluna@btinternet.com>
2011-06-15 15:52:02 +0100

    adding rails to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '2771d86f322a1c9d0477784e82e4e388e2613725a09ea18429d4acdb44d3f58c14e49dc7c2a2278139ecfff5e780acfdb3bf57848ae66771569ee5c7367f5701'
